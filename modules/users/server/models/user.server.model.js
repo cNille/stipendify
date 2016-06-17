28fbdc5d3cb73b5004d624db70a62cbddf78c883
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-06-17 17:55:40 +0200

    add stipendiumansvarig enum to user model

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -150,7 +150,7 @@ var UserSchema = new Schema({
   roles: {
     type: [{
       type: String,
-      enum: ['user', 'admin']
+      enum: ['user', 'admin', 'stipendiumansvarig']
     }],
     default: ['user'],
     required: 'Please provide at least one role'
