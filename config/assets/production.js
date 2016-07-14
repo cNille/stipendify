5e3d9b2383bf33ee7594e4425a0973768aa7ec29
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-07-14 16:59:27 +0200

    add filesaver dependency in production mode

--- config/assets/production.js
+++ config/assets/production.js
@@ -16,6 +16,7 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/file-saver/FileSaver.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
       ]
     },
